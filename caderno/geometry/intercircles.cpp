8e0305b82b1d0ab8e4f6d72f4e1bf74894af707c
antoniomsah <73652972+antoniomsah@users.noreply.github.com>
2022-08-31 16:29:45 -0300

    Refactor: algorithm simplified.

--- caderno/geometry/intercircles.cpp
+++ caderno/geometry/intercircles.cpp
@@ -5,32 +5,15 @@
  * Complexity: O(n^n log n) with high constant
  **/
 
-struct Cmp {
-	point r;
-	Cmp(point r) : r(r) {}
+bool cmp(point a, point b, point o){
+	a = a-o, b = b-o;
+	return make_tuple(-a.side(), 0) < make_tuple(-b.side(), a^b);
+}
 
-	int cuad(const point& a) const {
-		if(a.x > 0 and a.y >= 0) return 0;
-		if(a.x <= 0 and a.y > 0) return 1;
-		if(a.x < 0 and a.y <= 0) return 2;
-		if(a.x >= 0 and a.y < 0) return 3;
-		return -1;
-	}
-
-	bool cmp(const point& a, const point& b) const {
-		int c1 = cuad(a), c2 = cuad(b);
-		if(c1 == c2) return a.y*b.x < a.x*b.y;
-		return c1 < c2;
-	}
-
-	bool operator()(const point& a, const point& b){ return cmp(a-r,b-r); }
-};
-
-vector<point> intercircles(vector<circle> &c){
+vector<double> intercircles(vector<circle> &c){
 	vector<double> cover(c.size()+1); // area covered by at least k circles
 	for(int i=0; i<c.size(); i++){
 		int k=1;
-		Cmp s(c[i].o);
 		vector<pair<point,int>> p = {{c[i].o+point(1,0)*c[i].r,0}, {c[i].o-point(1,0)*c[i].r,0}};
 
 		for(int j=0; j<c.size(); j++){
@@ -40,13 +23,14 @@ vector<point> intercircles(vector<circle> &c){
 				auto v = c[i]^c[j];
 				if(v.size() == 2){
 					p.pb({v[0],1}); p.pb({v[1],-1});
-					if(s(v[1],v[0])) k++;
+					if(cmp(v[1],v[0],c[i].o)) k++;
 				}
 			}
 		}
 
+		point o = c[i].o;
 		sort(p.begin(), p.end(),
-				[&s](auto a, auto b){ return s(a.first, b.first); });
+				[&o](auto a, auto b){ return cmp(a.first,b.first,o); });
 	
 		for(int j=0; j<p.size(); j++){
 			point a = p[j? j-1: p.size()-1].first, // (j-1+p.size())%p.size()
