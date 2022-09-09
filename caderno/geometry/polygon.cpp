0c7f102e3ad1bc4821ae0e1f7d7ef5259688a5d8
antoniomsah <amsah@usp.br>
2022-09-09 16:54:55 -0300

    Refactor: rotating calipers algorithm.

--- caderno/geometry/polygon.cpp
+++ caderno/geometry/polygon.cpp
@@ -4,10 +4,9 @@ struct polygon {
 	polygon() {}
 	polygon(vector<point> q) : n(q.size()), p(q) {}
 
-	T calipers(){ // O(n)
+	T calipers(){ // O(n), caderno enrique
 		T ans=0;
 		for(int i=0,j=1; i<n; i++){
-			point v = p[(i+1)%n]-p[i];
 			while(((p[(i+1)%n]-p[i])^(p[(j+1)%n]-p[j])) > 0) j = (j+1)%n;
 			ans = max(ans, dist2(p[i],p[j])); // squared diameter.
 		}
