07fc4637c48d5f1252bbe43cb4fe1ebcc8233061
antoniomsah <amsah@usp.br>
2024-03-25 10:16:54 -0300

    Remove duplicate files

--- lib/geometry/segment.cpp
+++
@@ -1,7 +0,0 @@
-struct segment {
-	point p,q;
-
-	segment(point p, point q) : p(p), q(q) {};
-	bool inDisk(point r){ return (a-r)*(b-r) < EPS; }
-	bool has(point r) { return abs(area2(p,q,r)) < EPS and inDisk(r); }
-};
