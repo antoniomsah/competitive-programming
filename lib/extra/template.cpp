ce6699162054ae74f2c9055450cfa5775e2f5609
antoniomsah <amsah@usp.br>
2024-03-25 10:31:57 -0300

    Add template, pragma and bash hash files

---
+++ lib/extra/template.cpp
@@ -0,0 +1,22 @@
+// Template
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+#define int ll
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+
+void dbg_out() { cerr << endl; }
+template <typename H, typename... T>
+void dbg_out(H h, T... t) { cerr << ' ' << h; dbg_out(t...); }
+#define dbg(...) { cerr << #__VA_ARGS__ << ':'; dbg_out(__VA_ARGS__); }
+
+signed main() {
+	cin.tie(0)->sync_with_stdio(false);
+	cin.exceptions(cin.failbit);
+
+	return 0;
+}
